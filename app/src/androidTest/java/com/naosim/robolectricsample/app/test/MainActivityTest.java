5a2ad995e20b314232a224dd5d20306a878bb643
naosim <nao_pillows@yahoo.co.jp>
2014-05-24 12:07:49 +0900

    テスト追加 MainActivity.getTrue()が未実装なため、 ./gradlew build を実行すると、テストがレッド(エラー)になる。 テスト結果の詳細は app/build/test-report/debug/index.html で確認できる

--- app/src/androidTest/java/com/naosim/robolectricsample/app/test/MainActivityTest.java
+++ app/src/androidTest/java/com/naosim/robolectricsample/app/test/MainActivityTest.java
@@ -1,7 +1,20 @@
 package com.naosim.robolectricsample.app.test;
 
+import com.naosim.robolectricsample.app.MainActivity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by nao_pillows on 2014/05/24.
  */
+@RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
+    @Test
+    public void testGetTrue() throws Exception {
+        assertTrue(MainActivity.getTrue());
+    }
 }
